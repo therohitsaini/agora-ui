bf7feead931b984192258d27674fe63f02a4048a
Rohit suman <rohit.sangod74@gmail.com>
2025-10-25 17:10:30 +0530

    remove files

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
-  css: {
-    transformer: 'postcss', // ✅ disable lightningcss
-  },
+  plugins: [react(), tailwindcss(),],
   build: {
-    chunkSizeWarningLimit: 1600,
+    chunkSizeWarningLimit: 1600, 
   },
-  server: {
-    port: 5173,
-    host: true
-  }
 })
